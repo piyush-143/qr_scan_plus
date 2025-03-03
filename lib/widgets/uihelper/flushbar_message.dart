7225100ab82d200bf27d5516d6829267969e31b3
Piyush Dewangan <150065893+piyush-143@users.noreply.github.com>
2025-03-03 19:59:42 +0530

    flushbar message added

---
+++ lib/widgets/uihelper/flushbar_message.dart
@@ -0,0 +1,16 @@
+import 'package:another_flushbar/flushbar.dart';
+import 'package:flutter/material.dart';
+import 'color.dart';
+
+Future flushBarMessage(BuildContext context, String msg) {
+  return Flushbar(
+    message: msg,
+    margin: EdgeInsets.symmetric(horizontal: 25, vertical: 20),
+    duration: Duration(seconds: 2),
+    icon: Icon(Icons.info_outline_rounded, color: CustomColor.goldColor),
+    messageSize: 15,
+    backgroundColor: Colors.black12,
+    shouldIconPulse: false,
+    borderRadius: BorderRadius.circular(5),
+  ).show(context);
+}
