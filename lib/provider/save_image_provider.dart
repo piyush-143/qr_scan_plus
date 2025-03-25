2d5427b0daae339142ab0fc0cef385fc6d4ab4fa
Piyush Dewangan <150065893+piyush-143@users.noreply.github.com>
2025-03-25 23:36:19 +0530

    analyze image working

--- lib/provider/save_image_provider.dart
+++ lib/provider/save_image_provider.dart
@@ -1,7 +1,7 @@
 import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
-import 'package:image_gallery_saver_plus/image_gallery_saver_pluscker.dart';
+import 'package:image_gallery_saver_plus/image_gallery_saver_plus.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:screenshot/screenshot.dart';
 
