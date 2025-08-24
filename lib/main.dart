e2f82220a6506dd642a12b774dfbcbddb8958d16
Piyush Dewangan <150065893+piyush-143@users.noreply.github.com>
2025-08-24 11:45:29 +0530

    icon fixed

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
         ChangeNotifierProvider(create: (_) => QrCodeProvider()),
       ],
       child: MaterialApp(
-        title: 'Flutter Demo',
+        title: 'QR Scan+',
         theme: ThemeData(
           primarySwatch: Colors.blue,
           useMaterial3: true,
